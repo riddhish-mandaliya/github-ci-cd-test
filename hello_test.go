516dbd38c3c681c8bbd412b6ee5296a417587e44
Riddhish Mandaliya <rvmandaliya@gmail.com>
2022-02-23 00:04:30 +0530

    auto fmt test

--- hello_test.go
+++ hello_test.go
@@ -5,8 +5,8 @@ import (
 )
 
 func TestMain(t *testing.T) {
-	want := "Hello world"
-	got := PrefixHello("world")
+want := "Hello world"
+got := PrefixHello("world")
 	if got != want {
 		t.Errorf("Expecting %s, got %s", want, got)
 	}
